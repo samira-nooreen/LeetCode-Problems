558bd2c8d91dd60bf6cab8881fa9e47e5426851e
Samira <132845068+samira-nooreen@users.noreply.github.com>
2025-12-03 23:51:43 +0530

    Create 274. H-Index.java

---
+++ 274. H-Index.java
@@ -0,0 +1,17 @@
+/*274. H-Index */
+
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);  
+        int n = citations.length;
+        int h = 0;
+
+        for (int i = 0; i < n; i++) {
+            int papersWithAtLeast = n - i;
+            if (citations[i] >= papersWithAtLeast) {
+                return papersWithAtLeast;
+            }
+        }
+        return 0;
+    }
+}
