a997f0edc55c82cc439cf2c6059f1eb44108be2f
Samira <132845068+samira-nooreen@users.noreply.github.com>
2026-02-22 16:43:26 +0530

    Create 242. Valid Anagram.java

---
+++ 242. Valid Anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m = s.length();
+        int n=t.length();
+        if(m != n){
+            return false;
+        }
+
+        int count[] = new int[26];
+        for(int i =0; i<m ; i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for(int i = 0 ; i< 26 ;i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+  
+    }
+}
